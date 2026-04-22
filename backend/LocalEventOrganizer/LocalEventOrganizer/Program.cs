602db830ebf9e90a82a6ce349c8536c54b491a6c
0122345 <ntwarifiacre043@gmail.com>
2026-04-22 05:59:19 +0200

    DEBUG: adding Bearer for debuging 401 status where JWT was needed

--- backend/LocalEventOrganizer/LocalEventOrganizer/Program.cs
+++ backend/LocalEventOrganizer/LocalEventOrganizer/Program.cs
@@ -4,6 +4,7 @@ using LocalEventOrganizer.Hubs;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 if (File.Exists("/etc/secrets/.env"))
@@ -58,7 +59,27 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
